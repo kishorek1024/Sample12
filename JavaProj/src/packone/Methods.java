ede65b87d393192a71429441bb3ea842cd0251f6
kishore.k1024@gmail.com <kishore.k1024@gmail.com>
2019-12-08 14:28:26 +0530

    Commit2

--- JavaProj/src/packone/Methods.java
+++ JavaProj/src/packone/Methods.java
@@ -7,8 +7,8 @@ public class Methods {
 		System.out.println(a*b);		
 	}
 	public static void b1(){
-		int a=30; int b=60;
-		System.out.println(a+b);
+		int a=30; int b=60; int c=70;
+		System.out.println(a+b+c);
 	}
 	
 	public static void main(String[] args) {
